af22794c7bf826234c8ca22672487909675d323b
Hein Wai Yan Htet <heinwaiyanhtet2020@gmail.com>
2024-01-08 11:15:59 +0630

    added docker

---
+++ HWYHDotNetCore.ConsoleApp/AdoDotNetExamples/AdoDotNetExample.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HWYHDotNetCore.ConsoleApp.AdoDotNetExamples
+{
+    public class AdoDotNetExample{
+
+        public void Run()
+        {
+            Read();
+        }
+
+        private void Read()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+            {
+                
+            };
+
+            using SqlConnection connection = new SqlConnection(builder.ConnectionString);
+            connection.Open();
+
+
+        }
+
+        private void Edit(int id)
+        {
+
+        }
+
+    }
+}
